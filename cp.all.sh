4712801260ab0ca8996d10b6b1b49ba7f17fecf4
aquecola <aquecola@outlook.com>
2023-06-10 00:53:49 +0300

    Update dotfiles

---
+++ cp.all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cp ~/.config/waybar/config ~/dotfiles/.config/waybar/config -r
+cp ~/.config/waybar/style.css ~/dotfiles/.config/waybar/style.css -r
+
+cp ~/.config/sway/config ~/dotfiles/.config/sway/config -r
+
+cp ~/.config/wofi/config ~/dotfiles/.config/wofi/config -r
+cp ~/.config/wofi/style.css ~/dotfiles/.config/wofi/style.css -r
+
+cp ~/.config/kitty/kitty.conf ~/dotfiles/.config/kitty/kitty.conf -r
+
+cp ~/.config/"gtk-4.0"/settings.ini ~/dotfiles/.config/"gtk-4.0"/settings.ini -r
+cp ~/.config/"gtk-3.0"/settings.ini ~/dotfiles/.config/"gtk-3.0"/settings.ini -r
+
